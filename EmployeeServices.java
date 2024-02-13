fe7b2386878127b6422c80efa56cf9b2f004cce8
venki <venkannababu.tpg12@gmail.com>
2024-02-13 19:20:06 +0530

    test

---
+++ EmployeeServices.java
@@ -0,0 +1,13 @@
+package com.mypack;
+
+import java.util.List;
+
+
+ 
+public interface EmployeeServices {
+    List<Employee> getAllEmployee();
+    void save(Employee employee);
+    Employee getById(Long id);
+    void deleteViaId(long id);
+}
+
---
+++ ThyemeleaflombokdemoApplication.java
@@ -0,0 +1,13 @@
+package com.mypack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThyemeleaflombokdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThyemeleaflombokdemoApplication.class, args);
+	}
+
+}
